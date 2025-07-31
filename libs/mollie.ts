d8f16ecfb91bdfc986e1fa982f63554c7a59aa15
ouattaraissouf7507 <ouattaraissouf7507@gmail.com>
2025-07-31 09:47:23 +0000

    v03

--- libs/mollie.ts
+++ libs/mollie.ts
@@ -1,7 +1,7 @@
 import { createMollieClient } from "@mollie/api-client";
 
 const mollieClient = createMollieClient({
-  apiKey: process.env.MOLLIE_API_KEY!,
+  apiKey: process.env.MOLLIE_API_KEY || "",
 });
 
 export interface CreatePaymentData {

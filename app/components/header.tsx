3f7ce29000a229968580cf9ae7534ae8e7f8ba6b
ouattaraissouf7507 <ouattaraissouf7507@gmail.com>
2025-09-15 23:56:08 +0000

    lpaaaaqa

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -39,7 +39,7 @@ export default function Header() {
             </span>
           </Link>
 
-          {/* Desktop Navigation */}
+          {/* Desktop Navigation aa*/}
           <nav className="hidden lg:flex items-center space-x-8">
             {navLinks.map((link) => (
               <Link
